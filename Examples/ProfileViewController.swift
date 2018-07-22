3291386d84e43122d2790d5cf408897016af2bf0
Andrew Vavricek <avavricek@gmail.com>
2018-07-22 05:31:15 -0500

    update profile view

--- Examples/ProfileViewController.swift
+++ Examples/ProfileViewController.swift
@@ -14,20 +14,23 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var nickNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
     var indicator = UIActivityIndicatorView(frame: CGRect.init(x: 0, y: 0, width: 40, height: 40))
 
     
     override func viewWillAppear(_ animated: Bool) {
         nickNameTextField.text = UserDefaults.standard.string(forKey: "nickName") ?? ""
         
-        indicator.activityIndicatorViewStyle = .gray
+        indicator.activityIndicatorViewStyle = .white
         indicator.center = self.view.center
         self.view.addSubview(indicator)
     }
     
     @IBAction func saveProfile(_ sender: Any) {
         indicator.startAnimating()
-        
+        logInButton.isEnabled = false
+        createAccountButton.isEnabled = false
         guard let email = emailTextField.text, let oldNickName = UserDefaults.standard.string(forKey: "nickName") else { return }
         let userUpdate = UserUpdate(email: email, oldNickName: oldNickName, newNickName: nickNameTextField.text)
         updateUser(userUpdate) {
@@ -35,10 +38,15 @@ class ProfileViewController: UIViewController {
             defaults.set(self.nickNameTextField.text, forKey: "nickName")
             defaults.synchronize()
             self.navigationController?.popViewController(animated: true)
+            self.logInButton.isEnabled = true
+            self.createAccountButton.isEnabled = true
+            self.indicator.stopAnimating()
         }
     }
     
     @IBAction func login(_ sender: Any) {
+        logInButton.isEnabled = false
+        createAccountButton.isEnabled = false
         OktaAuth.clear()
         OktaAuth.login()
             .start(self)
@@ -47,6 +55,10 @@ class ProfileViewController: UIViewController {
             }
             .catch { error in
                 print("Failed to login")
+            }
+            .always {
+                self.logInButton.isEnabled = true
+                self.createAccountButton.isEnabled = true
             }
     }
 }
