c3a666b4a22ce00320e01fe6591999aff5d49a52
Jack Weber <jackweber@Jacks-MacBook-Pro.local>
2018-07-21 22:24:23 -0500

    Error fixing

---
+++ Examples/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  Examples
+//
+//  Created by Jack Weber on 7/21/18.
+//  Copyright © 2018 MapBox. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
